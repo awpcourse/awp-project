f21e96082f1df60ceb1132dd6cf5be1c0636c672
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2016-01-07 09:40:46 +0200

    Add tests for login view.

--- socialapp/tests.py
+++ socialapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class LoginViewTests(TestCase):
+    def test_index_redirects_to_login(self):
+        response = self.client.get(reverse('index'))
+        self.assertRedirects(response, '/login/?next=/')
+
+    def test_login_successful(self):
+        User.objects.create_user(username='Iulia', password='secret')
+        response = self.client.post(reverse('login'),
+                                    {'username': 'Iulia',
+                                     'password': 'secret'})
+        self.assertRedirects(response, '/')
+
+    def test_login_failure(self):
+        User.objects.create_user(username='Iulia', password='secret')
+        response = self.client.post(reverse('login'),
+                                    {'username': 'Iulia',
+                                     'password': 'fail'})
+        self.assertContains(response, 'Wrong username or password')
